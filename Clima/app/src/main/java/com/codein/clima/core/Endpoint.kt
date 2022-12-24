e7f45ab771dd1fc882f9fb34b0e04f100a4ee487
Jey Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2022-12-23 19:36:17 -0600

    funcionando

--- Clima/app/src/main/java/com/codein/clima/core/Endpoint.kt
+++ Clima/app/src/main/java/com/codein/clima/core/Endpoint.kt
@@ -1,6 +1,6 @@
 package com.codein.clima.core
 
 object Endpoint {
-    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    const val PRONOSTICO = "weather?lat={lat}&lon={lon}&appid=ce7c1b25bfe959d4b9132dacb3e928d7"
+    const val BASE_URL = "https://api.openweathermap.org/"
+    const val PRONOSTICO = "data/2.5/weather"
 }
--- Clima/app/src/main/java/com/codein/clima/data/network/ClimaApiclient.kt
+++ Clima/app/src/main/java/com/codein/clima/data/network/ClimaApiclient.kt
@@ -5,6 +5,7 @@ import com.codein.clima.data.model.ClimaRpModel
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -13,7 +14,8 @@ interface ClimaApiclient {
     @GET(Endpoint.PRONOSTICO)
     suspend fun getAllClima(
         @Query("lat") lat: Double,
-        @Query("lon") lon: Double
-    ) : Response<ClimaRpModel>
+        @Query("lon") lon: Double,
+        @Query("appid") appid: String = "ce7c1b25bfe959d4b9132dacb3e928d7"
+    ): Response<ClimaRpModel>
 
 }
